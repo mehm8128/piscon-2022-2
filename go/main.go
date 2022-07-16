12b07b4a2c73d7b20a1362e4f185e2755719d46e
mehm8128 <mehm8128@gmail.com>
2022-07-16 13:05:06 +0000

    ログ止めた

--- go/main.go
+++ go/main.go
@@ -17,7 +17,6 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"github.com/labstack/gommon/log"
 )
 
 const Limit = 20
@@ -241,11 +240,7 @@ func init() {
 func main() {
 	// Echo instance
 	e := echo.New()
-	e.Debug = true
-	e.Logger.SetLevel(log.DEBUG)
-
 	// Middleware
-	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	// Initialize
